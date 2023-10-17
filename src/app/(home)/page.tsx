e4868486b32a0fd20deac84ee22adc3fbc56a14c
Felipe Rocha <felipemota.rocha@gmail.com>
2023-10-17 20:28:23 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto esse mês!"
+      />
+    </div>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function Home() {
-  return <div></div>;
-}
