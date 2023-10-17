ff75f6f3010ffc18302e5c6e4bf90837646c7e96
Felipe Rocha <felipemota.rocha@gmail.com>
2023-10-16 21:17:47 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div
-      className="
-  bg-blue-500 text-red-500"
-    >
-      hello world
-    </div>
-  );
+  return <div></div>;
 }
