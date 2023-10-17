b045b43f706b06b6366a1c582fbb1f92d22e280e
Felipe Rocha <felipemota.rocha@gmail.com>
2023-10-16 21:10:12 -0300

    chore: add init migration and tailwind prettier plugin

---
+++ prisma/migrations/20231017000751_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "basePrice" DECIMAL(8,2) NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "discountPercentage" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
