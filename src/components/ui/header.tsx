a5dab2fb7370a27f7c8c9731e45b0d0666bf090c
Felipe Rocha <felipemota.rocha@gmail.com>
2023-10-17 22:25:15 -0300

    feat: ensure user goes to home page on logo click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -118,9 +118,11 @@ const Header = () => {
         </SheetContent>
       </Sheet>
 
-      <h1 className="text-lg font-semibold">
-        <span className="text-primary">FSW</span> Store
-      </h1>
+      <Link href="/">
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">FSW</span> Store
+        </h1>
+      </Link>
 
       <Button size="icon" variant="outline">
         <ShoppingCartIcon />
