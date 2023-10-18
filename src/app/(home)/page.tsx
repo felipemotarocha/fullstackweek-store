54e726f012e9f450785d2d13c1777431a61d3973
Felipe Rocha <felipemota.rocha@gmail.com>
2023-10-17 21:27:23 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -28,8 +28,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses!"
+      />
     </div>
   );
 }
