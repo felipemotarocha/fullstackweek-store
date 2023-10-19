aa3b5dbda41e74306136a1cecdf512d1f996e398
Mateus Vallim <mateus.vallim@usp.br>
2023-10-19 09:32:30 -0300

    fix: ensure the ProductItem have a fixed width when it's on ProductList

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -10,7 +10,11 @@ const ProductList = ({ products }: ProductListProps) => {
   return (
     <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
-          <ProductItem key={product.id} product={computeProductTotalPrice(product)} />
+        <ProductItem
+          key={product.id}
+          product={computeProductTotalPrice(product)}
+          className="w-[156px]"
+        />
       ))}
     </div>
   );
