810260f0f65ef788799d25f318371f2c613d4dee
Felipe Rocha <felipemota.rocha@gmail.com>
2023-10-19 21:10:40 -0300

    feat: add stripe webhook

--- env.d.ts
+++ env.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS {
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
     STRIPE_SECRET_KEY: string;
+    STRIPE_WEBHOOK_SECRET_KEY: string;
     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
   }
 }
---
+++ src/app/api/order/payment-success/route.ts
@@ -0,0 +1,37 @@
+import { prismaClient } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
+});
+
+export const POST = async (request: Request) => {
+  const signature = request.headers.get("stripe-signature");
+
+  if (!signature) {
+    return NextResponse.error();
+  }
+
+  const text = await request.text();
+
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET_KEY,
+  );
+
+  if (event.type === "checkout.session.completed") {
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+      event.data.object.id,
+      {
+        expand: ["line_items"],
+      },
+    );
+    const lineItems = sessionWithLineItems.line_items;
+
+    // CRIAR PEDIDO
+  }
+
+  return NextResponse.json({ received: true });
+};
