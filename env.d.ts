eee4310f1cdd2bf50a099ad68a36437983b15b0f
Felipe Rocha <felipemota.rocha@gmail.com>
2023-10-19 21:32:54 -0300

    feat: add HOST_URL

--- env.d.ts
+++ env.d.ts
@@ -6,5 +6,7 @@ declare namespace NodeJS {
     STRIPE_SECRET_KEY: string;
     STRIPE_WEBHOOK_SECRET_KEY: string;
     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
+    NEXTAUTH_SECRET: string;
+    HOST_URL: string;
   }
 }
--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -12,8 +12,8 @@ export const createCheckout = async (products: CartProduct[]) => {
   const checkout = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
-    success_url: "http://localhost:3000",
-    cancel_url: "http://localhost:3000",
+    success_url: process.env.HOST_URL,
+    cancel_url: process.env.HOST_URL,
     line_items: products.map((product) => {
       return {
         price_data: {
