96e60103fb97346b1f95d536d3a9c20d0082a6d7
Felipe Rocha <felipemota.rocha@gmail.com>
2023-10-23 21:39:04 -0300

    style: change order item text style

--- src/app/orders/components/order-item.tsx
+++ src/app/orders/components/order-item.tsx
@@ -48,7 +48,9 @@ const OrderItem = ({ order }: OrderItemProps) => {
         <AccordionItem value={order.id}>
           <AccordionTrigger>
             <div className="flex flex-col gap-1 text-left">
-              <p>Pedido com {order.orderProducts.length} produto(s)</p>
+              <p className="font-bold uppercase">
+                Pedido com {order.orderProducts.length} produto(s)
+              </p>
               <span className="text-sm opacity-60">
                 Feito em {format(order.createdAt, "d/MM/y 'às' HH:mm")}
               </span>
