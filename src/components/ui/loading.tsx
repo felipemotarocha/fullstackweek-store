a6184e085f619a3d27631d187abc86d297671560
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-10-26 00:51:29 -0300

    refactor: add spinner load in LoadingPage

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -1,11 +1,20 @@
-import { Skeleton } from "@/components/ui/skeleton";
+"use client";
+
+import { TailSpin } from "react-loader-spinner";
 
 const LoadingPage = () => {
   return (
     <div className="flex w-full flex-1 flex-col items-center p-[50%] px-20">
-      <div className="mt-12 w-[100%] animate-pulse flex-row items-center justify-center space-x-1 rounded-xl border p-6">
-        <Skeleton className="h-6 w-[100%] rounded-md bg-gray-300" />
-      </div>
+      <TailSpin
+        height="80"
+        width="80"
+        color="#5033C3"
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
     </div>
   );
 };
