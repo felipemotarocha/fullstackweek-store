15aa122dda8d080cfaa11f86b5ee429f1fbbf35b
Julio Cezar <captaingamergroot@gmail.com>
2023-10-27 09:29:22 -0300

    refactor: loading for all pages with Skeleton

--- src/app/catalog/loading.tsx
+++ src/app/catalog/loading.tsx
@@ -1,7 +1,23 @@
-import LoadingPage from "@/components/ui/loading";
+import { Badge } from '@/components/ui/badge'
+import { Skeleton } from '@/components/ui/skeleton'
+import { Shapes } from 'lucide-react'
 
-const Loading = () => {
-  return <LoadingPage />;
-};
+export default function CatalogLoading() {
+  return (
+    <div className="flex flex-col gap-8 p-5 lg:container lg:mx-auto">
+      <Badge variant="heading">
+        <Shapes size={16} />
+        Catálogo
+      </Badge>
 
-export default Loading;
+      <div className="grid grid-cols-2 gap-8 lg:grid-cols-3">
+        {[...Array(6)].map((x, i) => (
+          <Skeleton
+            className="flex h-[200px] w-full rounded-tl-lg rounded-tr-lg bg-accent"
+            key={i}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/app/category/[slug]/loading.tsx
+++ src/app/category/[slug]/loading.tsx
@@ -1,7 +1,21 @@
-import LoadingPage from "@/components/ui/loading";
+import { Badge } from '@/components/ui/badge'
+import { Skeleton } from '@/components/ui/skeleton'
 
-const Loading = () => {
-  return <LoadingPage />;
-};
+export default function CategoryLoading() {
+  return (
+    <div className="flex flex-col gap-8 p-5 lg:container lg:mx-auto">
+      <Badge variant="outline" className="border-0">
+        <Skeleton className="flex h-[40px] w-[100px] rounded-full bg-accent" />
+      </Badge>
 
-export default Loading;
+      <div className="grid grid-cols-2 gap-8 md:grid-cols-4 lg:grid-cols-6 lg:gap-10">
+        {[...Array(6)].map((x, i) => (
+          <Skeleton
+            className="flex h-[200px] w-full rounded-tl-lg rounded-tr-lg bg-accent"
+            key={i}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
