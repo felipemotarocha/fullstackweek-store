995d230a4a118224035dad2094fffee807d4e446
Felipe Rocha <felipemota.rocha@gmail.com>
2023-10-28 16:56:02 -0300

    feat: ensure home banners are clickable

--- src/app/(home)/components/promo-banner.tsx
+++ src/app/(home)/components/promo-banner.tsx
@@ -6,7 +6,7 @@ const PromoBanner = ({ alt, className, ...props }: ImageProps) => {
     <Image
       height={0}
       width={0}
-      className={cn("h-auto w-full px-5", className)}
+      className={cn("h-auto w-full", className)}
       sizes="100vw"
       alt={alt}
       {...props}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,7 @@ import ProductList from "../../components/ui/product-list";
 import SectionTitle from "../../components/ui/section-title";
 import PromoBanner from "./components/promo-banner";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -33,22 +34,23 @@ export default async function Home() {
   return (
     <>
       <div className="mx-auto max-w-[1920px]">
-        <Image
-          src="/deals-banner.png"
-          className="hidden h-auto w-full lg:block"
-          width={0}
-          height={0}
-          sizes="100vw"
-          alt="Até 55% de desconto esse mês!"
-        />
+        <Link href="/deals">
+          <PromoBanner
+            src="/deals-banner.png"
+            className="hidden h-auto w-full lg:block"
+            alt="Até 55% de desconto esse mês!"
+          />
+        </Link>
       </div>
 
       <div className="mx-auto flex flex-col gap-8 py-8 lg:container lg:gap-10">
-        <PromoBanner
-          src="/banner-home-01.png"
-          alt="Até 55% de desconto esse mês!"
-          className="lg:hidden"
-        />
+        <Link href="/deals">
+          <PromoBanner
+            src="/banner-home-01.png"
+            alt="Até 55% de desconto esse mês!"
+            className="px-5 lg:hidden"
+          />
+        </Link>
 
         <div className="px-5 lg:mt-2">
           <Categories />
@@ -60,17 +62,21 @@ export default async function Home() {
         </div>
 
         <div className="flex flex-col lg:flex-row">
-          <PromoBanner
-            src="/banner-home-02.png"
-            alt="Até 55% de desconto em mouses!"
-            className="w-0 flex-1"
-          />
+          <Link href="/category/mouses" className="flex flex-1">
+            <PromoBanner
+              src="/banner-home-02.png"
+              alt="Até 55% de desconto em mouses!"
+              className="w-0 flex-1 px-5"
+            />
+          </Link>
 
-          <PromoBanner
-            src="/banner-home-03.png"
-            alt="Até 55% de desconto em mouses!"
-            className="hidden w-0 flex-1 lg:block"
-          />
+          <Link href="/category/headphones" className="flex flex-1">
+            <PromoBanner
+              src="/banner-home-03.png"
+              alt="Até 55% de desconto em fones!"
+              className="hidden w-0 flex-1 lg:block"
+            />
+          </Link>
         </div>
 
         <div className="flex flex-col gap-3 lg:gap-5">
@@ -79,17 +85,21 @@ export default async function Home() {
         </div>
 
         <div>
-          <PromoBanner
-            src="/banner-home-03.png"
-            alt="Até 55% de desconto em mouses!"
-            className="lg:hidden"
-          />
+          <Link href="/category/headphones">
+            <PromoBanner
+              src="/banner-home-03.png"
+              alt="Até 55% de desconto em mouses!"
+              className="px-5 lg:hidden"
+            />
+          </Link>
 
-          <PromoBanner
-            src="/free-shipping-banner.png"
-            alt="Até 55% de desconto em mouses!"
-            className="hidden lg:block"
-          />
+          <Link href="/catalog">
+            <PromoBanner
+              src="/free-shipping-banner.png"
+              alt="Até 55% de desconto em mouses!"
+              className="hidden px-5 lg:block"
+            />
+          </Link>
         </div>
 
         <div className="flex flex-col gap-3 lg:gap-5">
