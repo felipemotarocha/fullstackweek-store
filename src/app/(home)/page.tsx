8111772e53a2023149a6452fd554231443726793
Felipe Rocha <felipemota.rocha@gmail.com>
2023-10-28 16:18:50 -0300

    feat: add first desktop banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { prismaClient } from "@/lib/prisma";
 import ProductList from "../../components/ui/product-list";
 import SectionTitle from "../../components/ui/section-title";
 import PromoBanner from "./components/promo-banner";
+import Image from "next/image";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -30,42 +31,56 @@ export default async function Home() {
   });
 
   return (
-    <div className="flex flex-col gap-8 py-8">
-      <PromoBanner
-        src="/banner-home-01.png"
-        alt="Até 55% de desconto esse mês!"
-      />
-
-      <div className="px-5">
-        <Categories />
-      </div>
-
-      <div>
-        <SectionTitle>Ofertas</SectionTitle>
-        <ProductList products={deals} />
-      </div>
-
-      <PromoBanner
-        src="/banner-home-02.png"
-        alt="Até 55% de desconto em mouses!"
-      />
-
-      <div>
-        <SectionTitle>Teclados</SectionTitle>
-        <ProductList products={keyboards} />
-      </div>
-
-      <div>
-        <PromoBanner
-          src="/banner-home-03.png"
-          alt="Até 55% de desconto em mouses!"
+    <>
+      <div className="mx-auto max-w-[1920px]">
+        <Image
+          src="/deals-banner.png"
+          className="hidden h-auto w-full lg:block"
+          width={0}
+          height={0}
+          sizes="100vw"
+          alt="Até 55% de desconto esse mês!"
         />
       </div>
 
-      <div>
-        <SectionTitle>Mouses</SectionTitle>
-        <ProductList products={mouses} />
+      <div className="container mx-auto flex flex-col gap-8 py-8">
+        <PromoBanner
+          src="/banner-home-01.png"
+          alt="Até 55% de desconto esse mês!"
+          className="lg:hidden"
+        />
+
+        <div className="px-5">
+          <Categories />
+        </div>
+
+        <div>
+          <SectionTitle>Ofertas</SectionTitle>
+          <ProductList products={deals} />
+        </div>
+
+        <PromoBanner
+          src="/banner-home-02.png"
+          alt="Até 55% de desconto em mouses!"
+        />
+
+        <div>
+          <SectionTitle>Teclados</SectionTitle>
+          <ProductList products={keyboards} />
+        </div>
+
+        <div>
+          <PromoBanner
+            src="/banner-home-03.png"
+            alt="Até 55% de desconto em mouses!"
+          />
+        </div>
+
+        <div>
+          <SectionTitle>Mouses</SectionTitle>
+          <ProductList products={mouses} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
