384c4c94922e789e5a9610801d790a52b11f1365
Felipe Rocha <felipemota.rocha@gmail.com>
2023-11-04 00:57:04 -0300

    feat: add active state to products button

--- src/app/(admin)/dashboard/components/sidebar.tsx
+++ src/app/(admin)/dashboard/components/sidebar.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { ListOrderedIcon, PackageIcon, PackageSearchIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const currentRoute = usePathname();
+
   return (
     <div className="flex h-full min-w-[240px] flex-col items-center gap-10 overflow-hidden border-r border-solid border-r-accent bg-background px-8 py-10">
       <h1 className="text-lg font-semibold">
@@ -9,7 +14,12 @@ const Sidebar = () => {
       </h1>
 
       <div className="flex w-full flex-col gap-3">
-        <Button variant="outline" className="flex w-full justify-start gap-2">
+        <Button
+          variant="outline"
+          className={`flex w-full justify-start gap-2 ${
+            currentRoute.includes("/products") && "bg-primary hover:bg-primary"
+          }`}
+        >
           <PackageIcon size={16} />
           Produtos
         </Button>
