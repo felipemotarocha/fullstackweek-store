ffe3d88ddaaeaf56fbac7a9342a40953b061b931
Felipe Rocha <felipemota.rocha@gmail.com>
2023-11-11 13:45:46 -0300

    feat: ensure products button have correct background on selected

--- src/app/(admin)/dashboard/components/sidebar.tsx
+++ src/app/(admin)/dashboard/components/sidebar.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+
 import {
   LayoutDashboardIcon,
   ListOrderedIcon,
   PackageIcon,
   PackageSearchIcon,
 } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const path = usePathname();
+
   return (
     <div className="flex min-w-[300px] flex-col items-center gap-8 border-r border-solid border-accent bg-background p-8">
       <h1 className="text-lg font-semibold">
@@ -19,10 +26,20 @@ const Sidebar = () => {
           Dashboard
         </Button>
 
-        <Button variant="outline" className="flex w-full justify-start gap-2">
-          <PackageIcon size={16} />
-          Produtos
-        </Button>
+        <Link href="/dashboard/products">
+          <Button
+            variant="outline"
+            className={`flex w-full justify-start gap-2
+          ${
+            path.includes("/products") &&
+            "bg-primary text-white hover:bg-primary"
+          }
+        `}
+          >
+            <PackageIcon size={16} />
+            Produtos
+          </Button>
+        </Link>
 
         <Button variant="outline" className="flex w-full justify-start gap-2">
           <ListOrderedIcon size={16} />
---
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -0,0 +1,7 @@
+import { usePathname } from "next/navigation";
+
+const ProductsPage = () => {
+  return <h1>products</h1>;
+};
+
+export default ProductsPage;
