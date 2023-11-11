eacc892997c79af7526c55ffce9724c9e4146207
Felipe Rocha <felipemota.rocha@gmail.com>
2023-11-11 13:46:50 -0300

    feat: ensure products are fetched on products page

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,6 +1,8 @@
-import { usePathname } from "next/navigation";
+import { prismaClient } from "@/lib/prisma";
 
-const ProductsPage = () => {
+const ProductsPage = async () => {
+  const products = await prismaClient.product.findMany();
+
   return <h1>products</h1>;
 };
 
