c6a9a07582de8a31f6e68e40011dd58bffda733b
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-03-21 20:45:20 -0300

    core: changed images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["fsw-store.s3.sa-east-1.amazonaws.com"],
+    domains: ["utfs.io"],
   },
   experimental: {
     serverActions: true,
